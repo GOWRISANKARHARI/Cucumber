42ead67266d33398d8b99907b2d5936e3f06b631
GOWRISANKARHARI <2k21ece032@kiot.ac.in>
2025-04-02 10:45:47 +0530

    update

--- cucumber_sample/src/test/java/runner/runnerTest.java
+++ cucumber_sample/src/test/java/runner/runnerTest.java
@@ -4,9 +4,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 	@RunWith(Cucumber.class)
-	@CucumberOptions(plugin={"pretty","json:target/cucumber-reports/Cucumber10.json"},features= "src/test/resources/com/ogin.feature",glue="stepDef")
+	@CucumberOptions(plugin={"pretty","json:target/cucumber-reports/Cucumber1.json"},features= "src/test/resources/com/ogin.feature",glue="stepDef")
 	//it is mainly used for the location of the process of the location of the report in junit so there are 3 type of the report of the process 
 	public class runnerTest {
 		
-
+//this is new 
 }
